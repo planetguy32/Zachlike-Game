b223bfb83f4c4f236d1c1e010f80c6d7676b075c
Justin <justinramiscal@gmail.com>
2018-06-06 13:55:08 -0700

    User can cancel an action by tapping on what they want canceled out. Still need to figure out how to get finger movements to spill over into the next grid

--- app/src/main/java/me/planetguy/notspacechem/MainActivity.java
+++ app/src/main/java/me/planetguy/notspacechem/MainActivity.java
@@ -1,21 +1,26 @@
 package me.planetguy.notspacechem;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener, View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
 
     private Button[][] grid = new Button[8][10];
-    private LinearLayout myLayout = null;
-    float x1, x2, y1, y2, dx, dy;
-    String direction;
+    //private LinearLayout myLayout = null;
+    private float x1, x2, y1, y2, dx, dy;
+    private String direction;
+    private GestureDetector gestureDetector;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +28,9 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         hideNavigationBar();
 
-        myLayout = findViewById(R.id.myLayout);
+        gestureDetector = new GestureDetector(this, new SingleTapConfirm());
+
+        //myLayout = findViewById(R.id.myLayout);
 
         // Get references to the buttons (the grid)
         for (int i = 0; i < 8; i++){
@@ -31,44 +38,54 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 String gridID = "grid_" + i + j;
                 int resID = getResources().getIdentifier(gridID, "id", getPackageName());
                 grid[i][j] = findViewById(resID);
-                //grid[i][j].setOnClickListener(this);
                 grid[i][j].setOnTouchListener(new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View view, MotionEvent motionEvent) {
 
-                        if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-                            x1 = motionEvent.getX();
-                            y1 = motionEvent.getY();
-                        } else if(motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-                            x2 = motionEvent.getX();
-                            y2 = motionEvent.getY();
-                            dx = x2-x1;
-                            dy = y2-y1;
-                        }
+                        if(gestureDetector.onTouchEvent(motionEvent)){
+                            // Single Tap - cancel out what you have drawn
+                            Log.d("CHECK_D", "cancel");
+                            ((Button) view).setText(" ");
 
-                        if(Math.abs(dx) > Math.abs(dy)) {
-                            if(dx>0){
-                                direction = "right";
-                                Log.d("CHECK_D", direction);
-                                ((Button) view).setText(">");
-                            }
-                            else{
-                                direction = "left";
-                                Log.d("CHECK_D", direction);
-                                ((Button) view).setText("<");
-                            }
                         } else {
-                            if(dy>0){
-                                direction = "down";
-                                Log.d("CHECK_D", direction);
-                                ((Button) view).setText("v");
-                            }
-                            else{
-                                direction = "up";
-                                Log.d("CHECK_D", direction);
-                                ((Button) view).setText("^");
+                            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                                x1 = motionEvent.getX();
+                                y1 = motionEvent.getY();
+                            } else if(motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+                                x2 = motionEvent.getX();
+                                y2 = motionEvent.getY();
+                                dx = x2-x1;
+                                dy = y2-y1;
                             }
 
+                            if(Math.abs(dx) > Math.abs(dy)) {
+                                if(dx>0){
+                                    // Move Right - draw right arrow
+                                    direction = "right";
+                                    Log.d("CHECK_D", direction);
+                                    ((Button) view).setText(">");
+                                }
+                                else{
+                                    // Move Left - draw left arrow
+                                    direction = "left";
+                                    Log.d("CHECK_D", direction);
+                                    ((Button) view).setText("<");
+                                }
+                            } else {
+                                if(dy>0){
+                                    // Move Down - draw down arrow
+                                    direction = "down";
+                                    Log.d("CHECK_D", direction);
+                                    ((Button) view).setText("v");
+                                }
+                                else{
+                                    // Move Up - draw up arrow
+                                    direction = "up";
+                                    Log.d("CHECK_D", direction);
+                                    ((Button) view).setText("^");
+                                }
+
+                            }
                         }
 
                         return true;
@@ -76,7 +93,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 });
             }
         }
-
 
     }
 
@@ -103,12 +119,12 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         return false;
     }
 
-    @Override
-    public void onClick(View view) {
-        if(!((Button) view).getText().toString().equals("")){
-            return;
-        }
 
-        ((Button) view).setText("8=D~~");
+    private class SingleTapConfirm extends SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent event) {
+            return true;
+        }
     }
 }
